6b2440dab515aa9e1ffbe2ffc034a039cd05f147
PARK-JONGMAN <pjm841@gmail.com>
2025-07-29 11:56:51 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1>KOMSA 외국인 어선원 안전교육</h1>
+      <p>Welcome to the Foreign Crew Safety Training Homepage</p>
+    </div>
+  );
+}
+
+export default App;
